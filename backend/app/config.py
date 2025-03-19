ee056aa31e40dd21a98dd9f141a26c7acc6fb37e
Boichuk Maksim <Boychuk.mk@gmail.com>
2025-03-19 18:25:28 +0200

    fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", )
 GENAI_API_KEY = os.getenv("GENAI_API_KEY")
-if not GENAI_API_KEY or not DATABASE_URL:
-    raise ValueError("Database URL ot Gemini API Key are not set in the environment variables")
+if not GENAI_API_KEY:
+    raise ValueError("Gemini API Key is not set in the environment variables")
